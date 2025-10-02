5e79500b23d99775cb14bcdce6e2bae60ff356f4
Gabriel <souzagabriel0006@gmail.com>
2025-10-02 10:05:05 -0300

    Tornando a migração idempotente

--- estagios/migrations/0015_create_superuser.py
+++ estagios/migrations/0015_create_superuser.py
@@ -8,9 +8,15 @@ def create_superuser(apps, schema_editor):
     admin_email = os.environ.get('ADMIN_EMAIL', 'sollencomktdigital@gmail.com')
     admin_password = os.environ.get('ADMIN_PASSWORD')
 
-    if admin_password and not User.objects.filter(username=admin_username).exists():
+    if User.objects.filter(username=admin_username).exists():
+        print(f'Superuser "{admin_username}" já existe')
+        return
+
+    if admin_password:
         User.objects.create_superuser(admin_username, admin_email, admin_password)
         print(f'Superuser "{admin_username}" criado')
+    else:
+        print('Senha do admin não definida - superuser não criado')
 
 
 class Migration(migrations.Migration):
