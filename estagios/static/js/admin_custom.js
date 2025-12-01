5317ce9abdc4008d493fd424149c34d1d36e2ccb
Gabriel <souzagabriel0006@gmail.com>
2025-12-01 10:31:33 -0300

    Alterando a lógica para a mudança de cor da linha

--- estagios/static/js/admin_custom.js
+++ estagios/static/js/admin_custom.js
@@ -3,7 +3,7 @@ window.addEventListener("load", function() {
         $('#result_list tbody tr').each(function() {
             // Verifica se a célula com a classe .field-restrito tem o ícone de 'icon-no'
             // IMPORTANTE: Verifique se o nome da classe é 'field-restrito' inspecionando o elemento
-            if ($(this).find('.field-restrito img[src*="icon-no"]').length > 0) {
+            if ($(this).find('.field-restrito img[src*="icon-yes"]').length > 0) {
                 $(this).css('background-color', '#ffe6e6'); // Pinta o fundo
                 $(this).find('td, th').css('color', '#a80000');
             }
